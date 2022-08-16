8d6da27a846ad7204aaeccd8b2c259ef93a8c8dc
SamarV-121 <samarvispute121@pm.me>
2022-08-16 13:52:15 +0530

    RMX2020: Link older libutils for PQ and dfps HALs
    * Abort message: 'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already owned'
    
    Change-Id: I8b014200f5d4ed8fe160f2176218785443b6e9a3

--- extract-files.sh
+++ extract-files.sh
@@ -66,6 +66,49 @@ fi
 # Initialize the helper for device
 setup_vendor "${DEVICE}" "${VENDOR}" "${ROOT}" true "${CLEAN_VENDOR}"
 
+function blob_fixup {
+    case "$1" in
+        lib/libsink.so)
+            "${PATCHELF}" --add-needed "libshim_vtservice.so" "${2}"
+            ;;
+        vendor/lib/hw/audio.primary.mt6768.so)
+            "${PATCHELF}" --replace-needed "libmedia_helper.so" "libmedia_helper-v30.so" "${2}"
+            ;; 
+        vendor/bin/hw/android.hardware.wifi@1.0-service-lazy-mediatek)
+            "${PATCHELF}" --replace-needed "libwifi-hal.so" "libwifi-hal-mtk.so" "${2}"
+            ;;
+        vendor/bin/hw/android.hardware.wifi@1.0-service-lazy-mediatek)
+            ;&
+        vendor/bin/hw/hostapd)
+            ;&
+        vendor/bin/hw/wpa_supplicant)
+            "${PATCHELF}" --add-needed "libcompiler_rt.so" ${2}
+            ;;
+        vendor/bin/hw/camerasloganserver)
+            ;&
+        vendor/lib/libmtkcam_stdutils.so)
+            ;&
+        vendor/lib64/libmtkcam_stdutils.so)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v29.so" "${2}"
+            ;;
+        vendor/bin/hw/camerahalserver)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
+            ;;
+        vendor/etc/init/android.hardware.bluetooth@1.0-service-mediatek.rc)
+            sed -i '/vts/Q' "$2"
+            ;;
+        vendor/etc/init/vendor.oppo.hardware.biometrics.fingerprint@2.1-service.rc)
+            sed -i '/cpuset/Q' "$2"
+            ;;
+        vendor/lib64/hw/dfps.mt6785.so)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
+            ;;
+        vendor/lib64/hw/vendor.mediatek.hardware.pq@2.3-impl.so)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
+            ;;
+    esac
+}
+
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
         "${KANG}" --section "${SECTION}"
 
